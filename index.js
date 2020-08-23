a05c716b829dd00c812798935619ea3974dd6ae1
Knar <knargr@gmail.com>
2020-08-23 20:08:44 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+      return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  static sanitize(str){
+    return str.replace(/[^-,'A-Za-z0-9 ]+/g, "");
+  }
+
+  static titleize(str){
+   let arr =  str.split(" ");
+  
+   for(let i= 0 ; i<arr.length; i++){
+    if(i===0){
+      arr[i]= arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    else if(arr[i]!= 'the' && arr[i]!= "an" && arr[i]!= "a" && arr[i]!= 'but' && arr[i]!= "of" && arr[i]!= 'and' && arr[i]!= "for" && arr[i]!= "at" && arr[i]!= "by" && arr[i]!= "from"){
+      arr[i]= arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+   }
+   return arr.join(" ");
+  }
 }
