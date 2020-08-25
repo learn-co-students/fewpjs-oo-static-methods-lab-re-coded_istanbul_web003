f0f4e76121d64b750d7d55ac221656ba9f91606b
ihab-sensei <ib-mashhadani@hotmail.com>
2020-08-25 04:12:25 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "");
+  }
+
+  static titleize(string) {
+    const wordsArray = string.split(" ");
+    const titleizedArray = [];
+    titleizedArray.push(this.capitalize(wordsArray[0]));
+    const exceptionsArray = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    for (let i = 1; i < wordsArray.length; i++) {
+      if (exceptionsArray.includes(wordsArray[i])) {
+        titleizedArray.push(wordsArray[i]);
+      } else {
+        titleizedArray.push(this.capitalize(wordsArray[i]));
+      }
+    }
+    return titleizedArray.join(" ");
+  }
 }
