f0277f917a9b4e9f297c4b1200b9547656f0bc69
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-10-12 16:12:01 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(str) {
+    let arr = [];
+    let unCapitalize = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let strSplit = str.split(' ');
+    for (let i = 0; i < strSplit.length; i++) {
+      if (i === 0 ) {
+        arr.push(this.capitalize(strSplit[i]));
+      } else {
+        if (unCapitalize.includes(strSplit[i])) {
+          arr.push(strSplit[i]);
+        } else {
+          arr.push(this.capitalize(strSplit[i]));
+        }
+      }
+    }
+    return arr.join(' ');
+  }
 }
