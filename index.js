c0356cc8df5e16ed98f04f40dc65511ba5979342
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-10-26 19:52:45 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
   //add static methods here
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'-'\s]+/g, "");
+  }
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static titleize(string) {
+    let splitWords = string.split(" ");
+    for (let i = 0; i < splitWords.length; i++) {
+      splitWords[0] = this.capitalize(splitWords[0]);
+      if (
+        splitWords[i] !== "a" &&
+        splitWords[i] !== "an" &&
+        splitWords[i] !== "but" &&
+        splitWords[i] !== "of" &&
+        splitWords[i] !== "and" &&
+        splitWords[i] !== "for" &&
+        splitWords[i] !== "at" &&
+        splitWords[i] !== "by" &&
+        splitWords[i] !== "and" &&
+        splitWords[i] !== "from" &&
+        splitWords[i] !== "the"
+      ) {
+        splitWords[i] = this.capitalize(splitWords[i]);
+      }
+    }
+    return splitWords.join(" ");
+  }
 }
