087e15dfb9ed37488470f54243275f6ed1b2f95b
Cihan_Uygur <magicihan89@gmail.com>
2020-11-01 00:01:47 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.replace(/^./, string[0].toUpperCase())
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'-'\s]+/g, "")
+  }
+
+  static titleize(string) {
+    const words = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "and",
+      "from",
+    ]
+    let array = string.split(" ")
+    for (let i = 0; i < array.length; i++) {
+      array[0] = this.capitalize(array[0])
+      if (!words.includes(array[i])) {
+        array[i] = this.capitalize(array[i])
+      }
+    }
+    return array.join(" ")
+  }
 }
